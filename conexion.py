086e6d8a8fbe5c807a50a5ffcc05b31b6c3cf64e
Jorge Cervantes <jorge.cervantesz@my.unitec.edu.mx>
2022-01-31 14:46:12 -0600

    PYTHO AdminUser

--- conexion.py
+++
@@ -1,8 +0,0 @@
-import mariadb
-
-def conect():
-    return mariadb.connect(host='localhost',
-                            user='root',
-                            password='12345',
-                            db='usuarios',
-                            port=3307,)
