b48087bac2d78064e1be27abf9084ac355763e71
Jorge Cervantes <jorge.cervantesz@my.unitec.edu.mx>
2022-03-10 18:02:26 -0600

    script sftp

--- stfpssh.py
+++ stfpssh.py
@@ -1,6 +1,10 @@
 import pysftp
  
-conexion = pysftp.Connection(host='192.168.1.153',username='root',password='')
-conexion.put('PGPSTFP.py','/root/PGPSTFP.py')
+cnopts = pysftp.CnOpts()
+cnopts.hostkeys = None
+
+
+conexion = pysftp.Connection(host='192.168.1.156',username='root',password='12345', cnopts=cnopts)
+conexion.put('C:/Users/beto_/Documents/ServicioSocial/SSHconection/scripts_python_ohio.pem','/root/scripts_python_ohio.pem')
 conexion.close()
 
