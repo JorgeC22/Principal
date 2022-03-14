5e621272e646961ba0daa3d17696edd5081f30de
Jorge Cervantes <jorge.cervantesz@my.unitec.edu.mx>
2022-03-14 17:57:53 -0600

    Script para verificacion de archivos

--- PGPparamikosftp.py
+++ PGPparamikosftp.py
@@ -1,11 +1,11 @@
-from paramiko import SSHClient, RSAKey
+from paramiko import SSHClient, RSAKey, AutoAddPolicy
 
 def extracccion():
     session = SSHClient()
-
+    session.set_missing_host_key_policy(AutoAddPolicy())
     session.load_system_host_keys()
 
-    key_file = RSAKey.from_private_key_file('nombre o ruta del archivo clave ssh')
+    key_file = RSAKey.from_private_key_file('')
 
     session.connect(
         hostname='',
@@ -22,21 +22,23 @@ def extracccion():
     list = sftp_client.listdir('/home/ec2-user/archivos_26_01_2022')
     list2 = []
     print(list)
-    for x in list:
-        cadena = x.find("PGP")
-        if cadena != -1:
-            list.append(x)
+    if list:
+        print('Existe una lista de archivos.')
+        for x in list:
+            cadena = x.find('.PGP')
+            if cadena != -1:
+                list2.append(x)
 
     print(list2)
-
+    
     if list2:
         print('Existe Archivos.')
         print(list2)
         for x in list2:
-            sftp_client.get('/home/ec2-user/archivos_26_01_2022/'+x,'C:/Users/beto_/Documents/ServicioSocial/ArchivoOpen/pruebas/'+x)
+            sftp_client.get('/home/ec2-user/archivos_26_01_2022/'+x,x)
     else:
         print('No existe archivos.')
-
+    
     sftp_client.close()
     session.close()
 
