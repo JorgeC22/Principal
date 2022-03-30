7d8522fbaf3e32ca186cc9493da593c367b21810
Jorge Cervantes <jorge.cervantesz@my.unitec.edu.mx>
2022-03-29 18:05:42 -0600

    MDT hash

--- MDT/conexion.py
+++ MDT/conexion.py
@@ -20,7 +20,7 @@ class dbMDT():
         try:
             self.cur = self.conn.cursor(dictionary=True, prepared=True)
             self.cur.execute("""
-                INSERT INTO mdt2 (Clave_de_Trx, Tipo_de_Trx, Fecha_Origen, Fecha_Posteo, Hora, Banco_Emisor, Banco_Adquirente, Numero_de_Cuenta, 
+                INSERT INTO mdt3 (Clave_de_Trx, Tipo_de_Trx, Fecha_Origen, Fecha_Posteo, Hora, Banco_Emisor, Banco_Adquirente, Numero_de_Cuenta, 
                         Cuenta_Token, Numero_de_Autorizacin, Monto, Numero_de_Referencia, CR, Origen_Respuesta, Red_Logica_Emisor, Red_Logica_Adquirente, 
                         PSEM, Clase_de_Tarjeta, Tipo_de_Tarjeta, Aut_Cap, Comercio, Origen_Mensaje, Protocolo, Terminal_Id, Terminal_Num, 
                         Flag_Service_Code, Terminal_Capability, Nombre_comercio, Tkn_Q1_S1, Tkn_Q1_S2, Tkn_Q2_S1, Tkn_C0_S1, Tkn_C0_S2, Tkn_C0_S3, 
@@ -29,11 +29,11 @@ class dbMDT():
                         Tkn_04_S2, Tkn_04_S3, Tkn_04_S4, Tkn_04_S5, Tkn_04_S6, Tkn_Q6_S1, Tkn_Q6_S2, Tkn_Q6_S3, Tkn_C6_S1, Tkn_C6_S2, Tkn_CE_S1, 
                         Tkn_CE_S2, Tkn_B2_S3, Tkn_B2_S4, Tkn_B2_S5, Tkn_B2_S6, Tkn_B2_S7, Tkn_B2_S8, Tkn_B2_S9, Tkn_B2_S10, Tkn_B2_S11, Tkn_B2_S12, 
                         Tkn_B2_S13, Tkn_B2_S14, Tkn_B2_S16, Tkn_B3_S2, Tkn_B3_S3, Tkn_B3_S6, Tkn_B3_S7, Tkn_B3_S8, Tkn_B3_S10, Tkn_B4_S1, Tkn_B4_S2, 
-                        Tkn_B4_S3, Tkn_B4_S4, Tkn_B4_S5, Tkn_B4_S6, Tkn_B4_S7, Tkn_B4_S8, Tkn_B4_S9, Tkn_B5_S1, Tkn_B5_S2, Tkn_B5_S3, Tkn_B5_S4, Tkn_B5_S5)
+                        Tkn_B4_S3, Tkn_B4_S4, Tkn_B4_S5, Tkn_B4_S6, Tkn_B4_S7, Tkn_B4_S8, Tkn_B4_S9, Tkn_B5_S1, Tkn_B5_S2, Tkn_B5_S3, Tkn_B5_S4, Tkn_B5_S5, cod_registro)
                         VALUES
                         (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
                         ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-                        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
                 """,params['tuplaDatos'])
         except mariadb.ProgrammingError as e:
             print(f"Error insertando registro: {e}")
@@ -43,15 +43,7 @@ class dbMDT():
     def existeRegistro(self, i):
         try:
             self.cur = self.conn.cursor(dictionary=True, prepared=True)
-            self.cur.execute("""
-                SELECT COUNT(*) as cuenta FROM mdt2 WHERE
-                    Fecha_Origen = ?
-                    and Numero_de_Cuenta = ?
-                    and Monto = ?
-                    and Numero_de_Referencia = ?
-                    and Tipo_de_Trx = ?
-                    and Tkn_B2_S5 = ?;
-            """, ([i[2],i[7],i[10],i[11],i[1],i[70]]))
+            self.cur.execute("SELECT COUNT(*) as cuenta FROM mdt3 WHERE cod_registro = '%s'; "% i[101])
             record = self.cur.fetchone()   
             self.cur.close()
             return record
@@ -59,3 +51,12 @@ class dbMDT():
             print(f"Error consultando existencia de registro: {e}")
         except mariadb.InterfaceError as e:
             print(f"Desconexión a BD log: {e}")
+
+def dbUsuario():
+    return mariadb.connect(
+        host="",
+        port= ,
+        user="",
+        passwd="",
+        database=""
+    )
--- MDT/controlador.py
+++ MDT/controlador.py
@@ -1,8 +1,12 @@
+from cgi import print_arguments
 from openpyxl import load_workbook
 from datetime import datetime
 from conexion import *
 import os
 import sys
+import bcrypt
+from models.usuario import usuario
+import hashlib
 
 def cargar(file, DB):
     now = datetime.now()
@@ -27,13 +31,17 @@ def cargar(file, DB):
         for valor in row:
             datosFila.append(valor.value) #se llena el arreglo correspondiente a la fila
             
-        #try:
-            #datosFila[2] = datetime.strptime(str(datosFila[2]), "%d/%m/%y").strftime("%Y-%m-%d")
-            #datosFila[3] = datetime.strptime(str(datosFila[3]), "%d/%m/%y").strftime("%Y-%m-%d")
-        #except BaseException as e:
-            #print(f"error al transformar fecha {e}")
-            #sys.exit(0)
-
+        try:
+            datosFila[2] = datetime.strptime(str(datosFila[2]), "%d/%m/%y").strftime("%Y-%m-%d")
+            datosFila[3] = datetime.strptime(str(datosFila[3]), "%d/%m/%y").strftime("%Y-%m-%d")
+        except BaseException as e:
+            print(f"error al transformar fecha {e}")
+            sys.exit(0)
+        
+        registro = str(datosFila)
+        bregistro = registro.encode()
+        hashFila = hashlib.new("sha256",bregistro)
+        datosFila.append(str(hashFila.hexdigest()))
         data.append(tuple(datosFila))
         j+=1
 
@@ -44,7 +52,6 @@ def cargar(file, DB):
         os.remove('./archivos/'+file.filename)
 
     totalInserciones = len(data)
-    
     u= 1
     for i in data:
         print(f"\rInsertando registro {u} de {totalInserciones}", end = '', flush=True)
@@ -52,9 +59,12 @@ def cargar(file, DB):
         #existe = DB.existeRegistro(i)
         #print(f'existe = {existe}')
         #DB.insertaRegistro(tuplaDatos = i)
-        u+=1
-        if DB.existeRegistro(i)['cuenta'] == 0:
+        #print(i)
+        #u+=1
+        if (DB.existeRegistro(i)['cuenta'] == 0):
             DB.insertaRegistro(tuplaDatos = i)
+            u+=1
+        else:
             u+=1
     now = datetime.now()
     print(f"\n{now.strftime('%Y-%m-%d %H:%M:%S')} terminada la carga en BD")
@@ -63,3 +73,63 @@ def main(archivo):
     file = archivo
     DB = dbMDT()
     cargar(file, DB)
+
+
+def login_usuario(usuario):
+    conexion = dbUsuario()
+    with conexion.cursor() as cursor:
+        cursor.execute("select * from usuarios where nombre_usuario = '"+usuario.nombre_usuario+"'")
+        datausuario = cursor.fetchall()
+        print(datausuario)
+        if datausuario:
+            for x in datausuario:
+                p = str(usuario.passs)
+                print(p)
+                passUsuario = p.encode("utf-8")
+                print(passUsuario)
+                p2 = str(x[2])
+                print(p2)
+                passBD = p2.encode("utf-8")
+                print(passBD)
+
+                if x[1] == usuario.nombre_usuario and bcrypt.checkpw(passUsuario, passBD):
+                    print("EXISTE")
+                    loginVericado = True
+                else:
+                    print("NO EXISTE")
+                    loginVericado = False
+        else:
+            loginVericado = False
+    
+    return loginVericado
+
+
+def loggin_user(username, password):
+    password = password.encode("utf-8")
+    conexxion = dbUsuario()
+    with conexxion.cursor() as cursor:
+        cursor.execute("select * from usuarios where nombre_usuario = '"+username+"'")
+        userdata = cursor.fetchall()
+        for x in userdata:
+            pass_BD = x[2].encode("utf-8")
+            if x[1] == username and bcrypt.checkpw(password, pass_BD):
+                #permiso = { "acceso": "true", "empresa": x[3] }
+                user = usuario(x[0],x[1],x[2])
+                break
+            else:
+                #permiso = { "acceso": "false", "empresa": None }
+                user = None
+    return user
+
+def get_by_id(id):
+    try:
+        conexion = dbUsuario()
+        cur = conexion.cursor()
+        cur.execute("select * from usuarios where id_usuario = '"+id+"'")
+        row = cur.fetchone()
+        if row != None:
+            return usuario(row[0], row[1], None)
+        else:
+            return None
+    except Exception as ex:
+        raise Exception(ex)
