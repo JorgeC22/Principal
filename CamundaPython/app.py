6da3133577a0e90f431c4a0bea4fa32a84dc4e2b
Jorge Cervantes <jorge.cervantesz@my.unitec.edu.mx>
2022-04-05 18:08:48 -0500

    camunda modificado

--- CamundaPython/app.py
+++ CamundaPython/app.py
@@ -12,14 +12,30 @@ def index():
     #flash('Mensaje de prueba!')
     return render_template('index.html')
 
-@app.route("/pagina1")
+@app.route("/pagina1", methods = ['POST'])
 def pagina1():
-    return render_template('pagina1.html')
+    idproceso = controlador.inicioProceso(request.form['nombre'],request.form['apellidoP'],request.form['apellidoM'])
+    datosP = controlador.getVariablesProceso(idproceso)
+    return render_template('pagina1.html', data=datosP)
+
+@app.route("/pagina2", methods = ['POST'])
+def pagina2():
+    datosP = controlador.getVariablesProceso(request.form['idproceso'])
+    return render_template('pagina2.html', data=datosP)
 
 @app.route("/acceso", methods = ['POST'])
 def acceso():
-    idproceso = controlador.inicioProceso(request.form['name'],int(request.form['age']))
-    return render_template('pagina1.html', data=idproceso)
+    #data = controlador.getVariablesProceso(request.form['idproceso'])
+    #data['verificacion'] = request.form['verificar']
+    #print(data)
+    #idtask = controlador.gettask(request.form['idproceso'])
+    #controlador.CompleteTask(idtask,data)
+    #return "Proceso Humano Completado"
+    return request.form['verificar']
+
+@app.route("/acceso2", methods = ['POST'])
+def acceso2():
+    return request.form['verificar']
 
 
 if __name__== "__main__":
--- CamundaPython/controlador.py
+++ CamundaPython/controlador.py
@@ -1,26 +1,62 @@
+from unicodedata import name
 from camunda.client.engine_client import EngineClient
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camunda.external_task.external_task_worker import ExternalTaskWorker
 import uuid
 import requests
+import json
 
 
-def inicioProceso(name,age):
+def inicioProceso(nombre,aPaterno,aMaterno):
     client = EngineClient()
-    resp_json = client.start_process(process_key="charge-var", variables={"nombre": name, "edad": age}, tenant_id="", business_key="")
+    resp_json = client.start_process(process_key="validacion_de_datos", 
+        variables={"nombre": nombre, "paterno": aPaterno, "materno": aMaterno}, tenant_id="", business_key="")
     idproceso = resp_json['id']
     print(idproceso)
-    print(resp_json)
     return idproceso
 
 
-def getProcess(idtask):
+def getVariablesProceso(idproceso):
+    baseUrl = "http://localhost:8080/engine-rest/process-instance/"+str(idproceso)+"/variables"
+    respuesta = requests.get(baseUrl)
+    variables_json = respuesta.json()
+    data = {
+        "idproceso": ""+idproceso,
+        "nombre": variables_json['nombre']['value'],
+        "paterno": variables_json['paterno']['value'],
+        "materno": variables_json['materno']['value']
+    }
+    return data
 
-    baseUrl = "http://localhost:8080/engine-rest/task/"+idtask+"/complete"
-    
-
-    respuesta = requests.post(baseUrl, json=datos)
-
+def gettask(idproceso):
+    baseUrl = "http://localhost:8080/engine-rest/task?processInstanceId="+str(idproceso)+""
+    respuesta = requests.get(baseUrl)
     como_json = respuesta.json()
-    print("La respuesta del servidor es: ")
     print(como_json)
+    for x in como_json:
+        idtask = x['id']
+ 
+    return idtask
+
+def CompleteTask(idtask,data):
+    baseUrl = "http://localhost:8080/engine-rest/task/"+str(idtask)+"/complete"
+    data = {
+    "variables": {
+            "nombre": {
+                "value": data['nombre']
+            },
+            "paterno": {
+                "value": data['paterno']
+            },
+            "materno": {
+                "value": data['materno']
+            },
+            "verificacion": {
+                "value": ""+data['verificacion']
+            }
+        }
+    }
+    respuesta = requests.post(baseUrl, json=data)
+
+    print("La respuesta del servidor es: ")
+    print("Proceso Completado")
